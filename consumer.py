0e2f26a982d7551e417f840274853acc1c796d19
Db-Lau <debbielau.hk@gmail.com>
2026-09-03 10:18:33 +0200

    add timescaledb

--- consumer.py
+++ consumer.py
@@ -1,7 +1,12 @@
 import random 
+import time
 
 def simulate_temp():
     temp = round(random.uniform(10,30), 2)
     return temp
 
-if __name__ = "__main__":
+if __name__ == "__main__":
+    while True:
+        temp = simulate_temp()
+        print(temp)
+        time.sleep(.5)
